22a1ac3cd1bbda0fab0599831b42a0018b3b6f3e
Michelle Alejandra Díaz Figueroa <midifi26@gmail.com>
2025-04-01 14:09:12 +0200

    Ejercicios clases Persona,Estudiante,Profesor

---
+++ script.js
@@ -0,0 +1,65 @@
+// Ejercicio POO 1
+
+// Crear la clase Persona, con propiedades nombre, edad y género, y el método obtDetalles(), que muestra por pantalla las propiedades de la persona.
+class Persona {
+    constructor(nombre, edad, genero){
+        this.nombre = nombre;
+        this.edad = edad;
+        this.genero = genero;
+    }
+    ObtDetalles(){
+        console.log(`Me llamo ${this.nombre}, tengo ${this.edad} y soy ${this.genero}`)
+    }
+};
+
+let misDatos= new Persona ('Michelle', 24, 'mujer');
+misDatos.ObtDetalles();
+
+
+// Crear la clase Estudiante, que hereda de Persona, e incluye las propiedades curso y grupo y el método registrar(), que muestre por pantalla el resultado.
+/*class Estudiante extends Persona{
+    constructor(nombre , edad , genero , curso , grupo){
+        super(nombre,edad,genero);
+        this.curso = curso;
+        this.grupo = grupo;
+    }
+   registrar(){
+    console.log(`Soy ${this.nombre}, tengo ${this.edad} soy ${this.genero}, curso ${this.curso} y estoy en el grupo ${this.grupo}.`);
+   } 
+};
+
+let informacion = new Estudiante('Mauricio', 20, 'Hombre','Grado Superior de Marketing', 'C');
+informacion.registrar();*/
+
+// Crear la clase Estudiante, que hereda de Persona, e incluye las propiedades curso y grupo y el método registrar(), que muestre por pantalla el resultado.
+class Estudiante extends Persona{
+    constructor(nombre , edad , genero , curso , grupo){
+        super(nombre,edad,genero);
+        this.curso = curso;
+        this.grupo = grupo;
+    }
+   registrar(){
+    console.log(`Soy ${this.nombre}, tengo ${this.edad} soy ${this.genero}, curso ${this.curso} y estoy en el grupo ${this.grupo}.`);
+   } 
+};
+
+let informacion = new Estudiante('Mauricio', 20, 'Hombre','Grado Superior de Marketing', 'C');
+informacion.registrar();
+
+
+// Crear la clase Estudiante, que hereda de Persona, e incluye las propiedades curso y grupo y el método registrar(), que muestre por pantalla el resultado.
+class Profesor extends Persona{
+    constructor(nombre , edad , genero , asignatura , nivel){
+        super(nombre , edad, genero);
+        this.asignatura = asignatura;
+        this.nivel = nivel;
+    }
+    asignar(){
+        console.log(`Me llamo ${this.nombre}, tengo ${this.edad} y soy ${this.genero}, imparto la asignatura de ${this.asignatura} en nivel ${this.nivel}.`);
+    }
+};
+
+let presentacion = new Profesor('Anibal', 45, 'Hombre', 'Quimica', 'Universitario');
+presentacion.asignar();
+
+// Crear los objetos y casos de prueba necesarios para comprobar el correcto funcionamiento de la jerarquía de clases.
